8b64ac3d5470a2614755e103d8dfa6e318f13225
jadx2 <delmarjaim@gmail.com>
2021-06-18 11:16:46 -0500

    Add new cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,31 +11,45 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="/images/sunset_festival.jpg"
-              text="Webpage created for the Sunset Festival following responsive design. Built with Html, Sass"
+              text="Webpage created for the Sunset Festival following responsive design. Built with Html, Sass."
               label="HTML & CSS"
               url="https://jadx2.github.io/concert_capstone"
               repo="https://github.com/jadx2/concert_capstone"
             />
             <CardItem
               src="/images/chefsattic.png"
-              text="A recipe blog to share your recipes and rate other ones. Built with Ruby on Rails, Sass, Rspec, Capybara"
+              text="A recipe blog to share your recipes and rate other ones. Built with Ruby on Rails, Sass, Rspec, Capybara."
               label="Ruby On Rails"
               url="https://sheltered-coast-24004.herokuapp.com"
               repo="https://github.com/jadx2/chefs_attic"
             />
             <CardItem
               src="/images/Screenshot-Newsweek.jpg"
-              text="A clone of the 'Newsweek' newsletter following a responsive design. Built with Html, Bootstrap"
+              text="A clone of the 'Newsweek' newsletter following a responsive design. Built with Html, Bootstrap."
               label="Bootstrap"
               url="https://jadx2.github.io/newsweek_bootstrap"
               repo="https://github.com/jadx2/newsweek_bootstrap"
             />
             <CardItem
               src="/images/bakery.png"
-              text="A web page created for Le Boulangerie bakery, using Javascript, functional programing, Html and CSS"
+              text="A web page created for Le Boulangerie bakery, using Javascript, functional programing, Html and CSS."
               label="HTML, CSS, Javascript"
               url="https://jadx2.github.io/restaurant-page/"
               repo="https://github.com/jadx2/restaurant-page"
+            />
+            <CardItem
+              src="/images/weather-app.png"
+              text="A weather app that can show in C or F. Written in Javascript and Bootstrap that makes calls to the OpenWatherMap API."
+              label="Bootstrap, Javascript, AJAX, Webpack"
+              url="https://jadx2.github.io/weather-app/"
+              repo="https://github.com/jadx2/weather-app"
+            />
+            <CardItem
+              src="/images/space-eagles.jpg"
+              text="Space shooter remembering that good old arcade games. Written in Javascript and Phaser 3, Webpack as a bundler and tested with Jest."
+              label="Javascript, Webpack, Phaser 3, Jest"
+              url="https://space-eagles.netlify.app/"
+              repo="https://github.com/jadx2/shooter-js"
             />
           </ul>
         </div>
