6908b425a66234b8dc1e7f307f9e37e9515c8c7b
jadx2 <delmarjaim@gmail.com>
2021-06-26 19:11:38 -0500

    Update

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,36 +5,17 @@ function CardItem(props) {
     <>
       <li className="cards__item">
         <div className="cards__item__container">
-          <figure
-            className="cards__item__pic-wrap"
-            data-category={props.label}
-          >
-            <img
-              className="cards__item__img"
-              alt="Sunset Festival"
-              src={props.src}
-            />
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
+            <img className="cards__item__img" alt="Sunset Festival" src={props.src} />
           </figure>
           <div className="cards__item__info">
-            <h5 className="cards__item__text">
-              {props.text}
-            </h5>
+            <h5 className="cards__item__text">{props.text}</h5>
             <div className="cards__item__links">
-              <a
-                href={props.url}
-                className="cards__item__link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
+              <a href={props.url} className="cards__item__link" target="_blank" rel="noreferrer">
+                LIVE DEMO
               </a>
-              <a
-                href={props.repo}
-                className="cards__item__link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github Repo
+              <a href={props.repo} className="cards__item__link" target="_blank" rel="noreferrer">
+                GITHUB REPO
               </a>
             </div>
           </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,14 +11,14 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="/images/sunset_festival.jpg"
-              text="Webpage created for the Sunset Festival following responsive design. Built with Html, Sass."
+              text="Webpage created for the Sunset Festival following responsive design. Built with Html, SCSS."
               label="HTML & CSS"
               url="https://jadx2.github.io/concert_capstone"
               repo="https://github.com/jadx2/concert_capstone"
             />
             <CardItem
               src="/images/chefsattic.png"
-              text="A recipe blog to share your recipes and rate other ones. Built with Ruby on Rails, Sass, Rspec, Capybara."
+              text="A recipe blog to share your recipes and rate other ones. Built with Ruby on Rails, SCSS, RSpec, Capybara."
               label="Ruby On Rails"
               url="https://sheltered-coast-24004.herokuapp.com"
               repo="https://github.com/jadx2/chefs_attic"
@@ -32,21 +32,21 @@ function Cards() {
             />
             <CardItem
               src="/images/bakery.png"
-              text="A web page created for Le Boulangerie bakery, using Javascript, functional programing, Html and CSS."
+              text="A web page created for Le Boulangerie bakery, using JavaScript, Functional Programing, Html and CSS."
               label="HTML, CSS, Javascript"
               url="https://jadx2.github.io/restaurant-page/"
               repo="https://github.com/jadx2/restaurant-page"
             />
             <CardItem
               src="/images/weather-app.png"
-              text="A weather app that can show in C or F. Written in Javascript and Bootstrap that makes calls to the OpenWatherMap API."
+              text="A weather app that can show in C or F. Written in JavaScript and Bootstrap that makes calls to the OpenWatherMap API."
               label="Bootstrap, Javascript, AJAX, Webpack"
               url="https://jadx2.github.io/weather-app/"
               repo="https://github.com/jadx2/weather-app"
             />
             <CardItem
               src="/images/space-eagles.jpg"
-              text="Space shooter remembering that good old arcade games. Written in Javascript and Phaser 3, Webpack as a bundler and tested with Jest."
+              text="Space shooter remembering that good old arcade games. Written in JavaScript and Phaser 3, Webpack as a bundler and tested with Jest."
               label="Javascript, Webpack, Phaser 3, Jest"
               url="https://space-eagles.netlify.app/"
               repo="https://github.com/jadx2/shooter-js"
